19778b22c22a0fb302f4e7c6b576d8ed2e223571
ParkSuJeong <sujeong9158@gmail.com>
2022-07-13 16:19:15 +0900

    [Add] nest 기본 세팅, config/env, 예외처리, cors 설정

---
+++ back/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './pipes/HttpExceptionFilter.filter';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [{
+    provide: APP_FILTER,
+    useClass: HttpExceptionFilter,
+  },],
+})
+export class AppModule {}
