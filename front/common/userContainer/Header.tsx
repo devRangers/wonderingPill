6ba382291cdc8166f8daf12fdb6e9cd17c04c20f
rnrn99 <giu0213@naver.com>
2022-07-14 10:48:07 +0900

    [Fix] url 상수화

--- front/common/userContainer/Header.tsx
+++ front/common/userContainer/Header.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { SUB_COLOR } from "@utils/constant";
+import { SUB_COLOR, ROUTE } from "@utils/constant";
 import { AiOutlineLeft } from "react-icons/ai";
 import { IconBtn, LinkBtn } from "./Container.style";
 
 function Header() {
   const router = useRouter();
-  const isLoginPage = router.asPath === "/login";
+  const isLoginPage = router.asPath === ROUTE.LOGIN.link;
 
   return (
     <>
@@ -16,7 +16,7 @@ function Header() {
         </IconBtn>
       </Link>
 
-      <Link href={isLoginPage ? "/register" : "/login"}>
+      <Link href={isLoginPage ? ROUTE.REGISTER.link : ROUTE.LOGIN.link}>
         <LinkBtn>
           {isLoginPage ? "회원가입 바로가기" : "로그인 바로가기"}
         </LinkBtn>
--- front/common/utils/constant.ts
+++ front/common/utils/constant.ts
@@ -1,3 +1,12 @@
 export const MAIN_COLOR = "#A8C0EA";
 export const SUB_COLOR = "#3C5A93";
 export const BUTTON_COLOR = "#9ADCDD";
+
+export const ROUTE = {
+  LOGIN: {
+    link: "/login",
+  },
+  REGISTER: {
+    link: "/register",
+  },
+};
