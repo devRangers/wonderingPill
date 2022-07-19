73d7658b4501bd101c17383f5492c61ca15961f5
rnrn99 <giu0213@naver.com>
2022-07-19 15:38:50 +0900

    [Fix] resize event 삭제

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,32 +1,15 @@
 import "../styles/globals.css";
 import "../styles/reset.css";
 import type { AppProps } from "next/app";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { Provider as StyletronProvider } from "styletron-react";
 import { styletron } from "@utils/styletron";
 import Head from "next/head";
 
-const setScreenSize = () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-
-  useEffect(() => {
-    setScreenSize();
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", () => setScreenSize());
-
-    return () => {
-      window.removeEventListener("resize", () => setScreenSize());
-    };
-  }, []);
 
   return (
     <StyletronProvider value={styletron}>
