5016626b7abed1cc2e89ede936ca31f7215f22ad
rnrn99 <giu0213@naver.com>
2022-07-21 10:46:49 +0900

    [Add] token 보내기 시도(추후 백엔드로 보낼 예정)

--- front/components/account/Recaptcha.tsx
+++ front/components/account/Recaptcha.tsx
@@ -1,18 +1,35 @@
-import { useState, useEffect, useCallback } from "react";
+import { useCallback } from "react";
 import {
   GoogleReCaptchaProvider,
   GoogleReCaptcha,
 } from "react-google-recaptcha-v3";
+import { useMutation } from "react-query";
+
+const getRecaptchaResult = async (token: string) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_RECAPTCHA_URL}?secret=${process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}&response=${token}`,
+    {
+      method: "POST",
+    },
+  );
+  const result = await res.json();
+  return result;
+};
 
 function Recaptcha() {
-  const [token, setToken] = useState("");
+  const recaptchaMutation = useMutation(getRecaptchaResult, {
+    onSuccess: (data, variables) => {
+      console.log(data);
+    },
+  });
 
   const verifyToken = useCallback((data: string) => {
-    setToken(data);
+    recaptchaMutation.mutate(data);
   }, []);
 
   return (
-    <GoogleReCaptchaProvider reCaptchaKey="6Lel-wQhAAAAAB55a0ZHQoZNaBT68ZMVMyPGJP_X">
+    <GoogleReCaptchaProvider
+      reCaptchaKey={`${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}>
       <GoogleReCaptcha onVerify={verifyToken} />
     </GoogleReCaptchaProvider>
   );
