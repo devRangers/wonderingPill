b005a858f4b1d82cf3cd30712d6305bf2959536e
Malza0408 <parkjh04080@gmail.com>
2022-07-22 15:03:00 +0900

    [Fix] 쓰지 않는 코드 삭제

--- front/components/register/Authentication.tsx
+++ front/components/register/Authentication.tsx
@@ -170,7 +170,6 @@ function Authentication({
       handleSetApplySelfAuth(
         phoneNumberFormik.getFieldProps("phoneNumber").value,
       );
-      console.log("asd");
 
       setAuthSelf((cur) => {
         return {
