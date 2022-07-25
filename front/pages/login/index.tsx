78e6c836585266330089af60afad81741e00224f
rnrn99 <giu0213@naver.com>
2022-07-25 18:35:07 +0900

    [Add] bot일 경우 form 접근 제한

--- front/pages/login/index.tsx
+++ front/pages/login/index.tsx
@@ -1,30 +1,36 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Container from "@userContainer/Container";
 import LoginForm from "@loginComp/LoginForm";
 import Botd, { botDetect } from "@lib/botd/script";
 
 const LoginPage: NextPage = () => {
   const router = useRouter();
+  const [isBot, setIsBot] = useState(true);
 
   return (
     <Botd
       onLoad={() => {
-        Object.defineProperty(navigator, "userAgent", {
-          value:
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/90.0.4430.93 Safari/537.36",
-          configurable: true,
-        });
+        // Object.defineProperty(navigator, "userAgent", {
+        //   value:
+        //     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/90.0.4430.93 Safari/537.36",
+        //   configurable: true,
+        // });
         botDetect().then((result: any) => {
           console.log(result);
-          // if (result.bot.automationTool.probability === 1) {
-          //   router.push("/");
-          // }
+          if (result.bot.automationTool.probability === 1) {
+            router.push("/");
+          } else {
+            setIsBot(false);
+          }
         });
       }}>
-      <Container>
-        <LoginForm />
-      </Container>
+      {!isBot && (
+        <Container>
+          <LoginForm />
+        </Container>
+      )}
     </Botd>
   );
 };
