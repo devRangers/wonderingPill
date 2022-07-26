003e6b1b3258dd2ac5361dcc972a4f117ea9bf89
Malza0408 <parkjh04080@gmail.com>
2022-07-26 17:47:28 +0900

    [Add] mainpage logo Image

--- front/components/main/Main.style.ts
+++ front/components/main/Main.style.ts
@@ -3,5 +3,18 @@ import { styled } from "styletron-react";
 
 export const MainContainer = styled("div", (props: { $bgColor: string }) => ({
   backgroundColor: props.$bgColor,
-  minHeight: `calc(100vh - (${HEADER_HEIGHT} + ${FOOTER_HEIGHT}))`,
+  height: `calc(100vh - (${HEADER_HEIGHT} + ${FOOTER_HEIGHT}))`,
 }));
+
+export const MainContent = styled("div", {
+  height: "100%",
+  display: "grid",
+  gridTemplateRows: "1fr 4fr",
+});
+
+export const ImageSection = styled("section", {
+  position: "relative",
+  margin: "0 auto",
+  width: "60%",
+});
+export const MainSection = styled("section", {});
--- front/components/main/Main.tsx
+++ front/components/main/Main.tsx
@@ -1,9 +1,30 @@
 import { MAIN_COLOR } from "@utils/constant";
+import Image from "next/image";
 import React from "react";
-import { MainContainer } from "./Main.style";
+import {
+  ImageSection,
+  MainContainer,
+  MainContent,
+  MainSection,
+} from "./Main.style";
 
 function Main() {
-  return <MainContainer $bgColor={MAIN_COLOR}></MainContainer>;
+  return (
+    <MainContainer $bgColor={MAIN_COLOR}>
+      <MainContent>
+        <ImageSection>
+          <Image
+            src="/images/register_logo.png"
+            alt="wondering-pill-logo"
+            layout="fill"
+            objectFit="cover"
+            priority={true}
+          />
+        </ImageSection>
+        <MainSection></MainSection>
+      </MainContent>
+    </MainContainer>
+  );
 }
 
 export default Main;
