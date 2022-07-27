6a9aa2bacc0c1f568a75286d71c4c6f989622725
rnrn99 <giu0213@naver.com>
2022-07-27 16:15:58 +0900

    [Add] find email result page UI

--- front/pages/account/email/result.tsx
+++ front/pages/account/email/result.tsx
@@ -1,9 +1,55 @@
 import type { NextPage } from "next";
+import { isWideDevice } from "@utils/isWideDevice";
 import { MAIN_COLOR, SUB_COLOR, BUTTON_COLOR } from "@utils/constant";
-import { Container } from "@accountComp/result/Result.style";
+import {
+  Container,
+  TitleContainer,
+  Title,
+  SubTitle,
+  ResultContainer,
+  Description,
+  ResultBox,
+  InfoContaniner,
+  InfoTitle,
+  BtnContainer,
+  LinkButton,
+} from "@accountComp/result/Result.style";
 
 const FindEmailResultPage: NextPage = () => {
-  return <Container $bgColor={MAIN_COLOR}>result</Container>;
+  const isWide = isWideDevice();
+
+  return (
+    <Container $bgColor={MAIN_COLOR}>
+      <TitleContainer>
+        <Title $txtColor={SUB_COLOR}>계정 찾기</Title>
+        <SubTitle>회원님의 계정 찾기가 완료되었습니다.</SubTitle>
+      </TitleContainer>
+      <ResultContainer>
+        <Description>
+          다음 정보로 가입된 계정이
+          <br /> 총 1개 있습니다.
+        </Description>
+        <ResultBox $isWide={isWide}>
+          <InfoContaniner $isWide={isWide}>
+            <InfoTitle>이름</InfoTitle>
+            <p>테스트계정</p>
+          </InfoContaniner>
+          <InfoContaniner $isWide={isWide}>
+            <InfoTitle>계정</InfoTitle>
+            <p>sujeong9158@gmail.com</p>
+          </InfoContaniner>
+        </ResultBox>
+      </ResultContainer>
+      <BtnContainer $isWide={isWide}>
+        <LinkButton $isWide={isWide} $btnColor={BUTTON_COLOR}>
+          로그인 바로가기
+        </LinkButton>
+        <LinkButton $isWide={isWide} $btnColor={BUTTON_COLOR}>
+          비밀번호 찾기
+        </LinkButton>
+      </BtnContainer>
+    </Container>
+  );
 };
 
 export default FindEmailResultPage;
