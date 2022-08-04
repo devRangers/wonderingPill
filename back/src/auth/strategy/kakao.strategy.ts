8928ef31c821af1f17d658f4acffebbed1b2ca85
ParkSuJeong74 <sujeong9158@gmail.com>
2022-08-04 21:48:09 +0900

    [Add] kakao login api 생성, 콜수 변환, type 설정, test 필요

---
+++ back/src/auth/strategy/kakao.strategy.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import * as argon from 'argon2';
+import { Profile, Strategy } from 'passport-kakao';
+import { AuthService } from '../auth.service';
+import { KakaoLoginDto } from '../dto';
+
+@Injectable()
+export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
+  constructor(private readonly authService: AuthService) {
+    super({
+      clientID: process.env.KAKAO_REST_API_KEY,
+      clientSecret: process.env.KAKAO_SECRET_KEY,
+      callbackURL: process.env.KAKAO_REDIRECT_URI,
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done,
+  ) {
+    const password = await argon.hash(profile.id);
+    const payload: KakaoLoginDto = {
+      name: profile._json.properties.nickname,
+      email: profile._json.kakao_account.email,
+      profileImg: profile._json.properties.profile_image
+        ? profile._json.properties.profile_image
+        : undefined,
+      birth:
+        profile._json.kakao_account.birthyear +
+        profile._json.kakao_account.birthday,
+      password,
+      accessToken,
+      refreshToken,
+    };
+
+    done(null, payload);
+  }
+}
--- back/src/common/guards/index.ts
+++ back/src/common/guards/index.ts
@@ -1,3 +1,4 @@
+export * from './kakao.guard';
 export * from './local.access.guard';
 export * from './local.refresh.guard';
 export * from './recaptcha.guard';
