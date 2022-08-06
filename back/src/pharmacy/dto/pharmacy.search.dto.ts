432adf8abf3e14db0eee57b1ec7233d034ffeae2
Shin-GC <tlsrhkdcjs12@naver.com>
2022-08-06 13:51:46 +0900

    [Add] Pharmacy 및 Bookmark Module 추가

--- back/src/pharmacy/dto/pharmacy.search.dto.ts
+++ back/src/pharmacy/dto/pharmacy.search.dto.ts
@@ -1,17 +1,21 @@
-import { IsString, IsNumber, IsOptional } from 'class-validator';
-import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class PharmacyQueryDto {
+  @ApiProperty()
   @IsOptional()
   @IsString()
   readonly phone?: string;
+  @ApiProperty()
   @IsOptional()
   @IsString()
   readonly name?: string;
+  @ApiProperty()
   @IsOptional()
   @IsString()
   readonly address?: string;
+  @ApiProperty()
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
