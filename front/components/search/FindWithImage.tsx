d043232aa7d76f4880b2cff29b7fe872d54c4ce1
Malza0408 <parkjh04080@gmail.com>
2022-08-09 00:04:17 +0900

    [Fix] search/image box padding

--- front/components/search/FindWithImage.tsx
+++ front/components/search/FindWithImage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Image from "next/image";
 import {
   FOOTER_HEIGHT,
@@ -13,8 +13,8 @@ import {
   Description,
   DescriptionContainer,
   DescriptionContent,
-  DescriptionFirstBox,
-  DescriptionSecondBox,
+  DescriptionBox as FirstBox,
+  DescriptionBox as SecondBox,
   DescriptionTitle,
   FindWithImageContainer,
   ImageWrapper,
@@ -60,19 +60,19 @@ function FindWithImage() {
                 <TitleLine $bgColor={MAIN_COLOR} />
                 <Title>사진으로 찾기 이용 방법</Title>
               </DescriptionTitle>
-              <DescriptionFirstBox $bgColor={MAIN_COLOR}>
+              <FirstBox $bgColor={MAIN_COLOR}>
                 <Numbering>1</Numbering>
                 <DescriptionContent>
                   위 일러스트를 클릭하고 알고 싶은 알약 사진을 찍으세요!
                 </DescriptionContent>
-              </DescriptionFirstBox>
-              <DescriptionSecondBox $bgColor={MAIN_COLOR}>
+              </FirstBox>
+              <SecondBox $bgColor={MAIN_COLOR}>
                 <Numbering>2</Numbering>
                 <DescriptionContent>
                   머신러닝으로 알아낸 알약 이름과
                   <br /> 성분 등을 알아보세요!
                 </DescriptionContent>
-              </DescriptionSecondBox>
+              </SecondBox>
             </Description>
           </DescriptionContainer>
         </FindWithImageContainer>
