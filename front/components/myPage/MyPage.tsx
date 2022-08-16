39ebd8c4e978f00194de8ef350714b409844deb7
Malza0408 <parkjh04080@gmail.com>
2022-08-16 15:55:46 +0900

    [Add] 복용약 컨테이너

--- front/components/myPage/MyPage.tsx
+++ front/components/myPage/MyPage.tsx
@@ -1,15 +1,10 @@
-import Image from "next/image";
-import {
-  BUTTON_COLOR as BORDER_COLOR,
-  FOOTER_HEIGHT,
-  FULL_HEIGHT,
-  HEADER_HEIGHT,
-  MAIN_COLOR,
-} from "@utils/constant";
+import { BUTTON_COLOR as BORDER_COLOR } from "@utils/constant";
 import {
   ContentClip,
   ContentContainer,
   InnerContainer,
+  MedicineBadgeContainer,
+  PharmarcyContainer,
   Profile,
   SocialLoginState,
   UserInfo,
@@ -23,6 +18,17 @@ import {
 } from "./MyPage.style";
 import Container from "common/container/Container";
 import Capture from "common/capture/Capture";
+import { useState } from "react";
+import Medicine from "./medicine/Medicine";
+import { MedicineBadge } from "./medicine/Medicine.style";
+import Pharmarcy from "./pharmarcy/Pharmarcy";
+
+const medicinesName: { [key in string]: string } = {
+  name1: "가스모틴정",
+  name2: "가스모틴정_2",
+  name3: "가스모틴정_3",
+  name4: "가스모틴정_4",
+};
 
 function MyPage() {
   return (
@@ -55,9 +61,18 @@ function MyPage() {
         </UserInfoContainer>
         <ContentContainer $borderColor={BORDER_COLOR}>
           <ContentClip $bgColor={BORDER_COLOR}>복용약</ContentClip>
+          <MedicineBadgeContainer>
+            {Object.entries(medicinesName).map(([key, value], index) => (
+              <Medicine key={key} name={value} />
+            ))}
+          </MedicineBadgeContainer>
         </ContentContainer>
         <ContentContainer $borderColor={BORDER_COLOR}>
           <ContentClip $bgColor={BORDER_COLOR}>관심 약국</ContentClip>
+          <PharmarcyContainer>
+            <Pharmarcy></Pharmarcy>
+            <Pharmarcy></Pharmarcy>
+          </PharmarcyContainer>
         </ContentContainer>
       </InnerContainer>
     </Container>
---
+++ front/components/myPage/medicine/Medicine.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BUTTON_COLOR as BORDER_COLOR, MAIN_COLOR } from "@utils/constant";
+import { AlarmButton, MedicineBadge } from "./Medicine.style";
+
+interface MedicineProp {
+  name: string;
+}
+
+function Medicine({ name }: MedicineProp) {
+  return (
+    <MedicineBadge $borderColor={BORDER_COLOR}>
+      <p>{name}</p>
+      <AlarmButton $bgColor={MAIN_COLOR}>알림 수정하기</AlarmButton>
+    </MedicineBadge>
+  );
+}
+
+export default Medicine;
