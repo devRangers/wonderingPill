0bd1a5a5aa3ce8faa6f80a79db90ad6a5bc0a777
Malza0408 <parkjh04080@gmail.com>
2022-08-18 14:58:03 +0900

    [Add] slick 추가

--- front/components/myPage/MyPage.tsx
+++ front/components/myPage/MyPage.tsx
@@ -1,12 +1,15 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Container from "@container/Container";
 import Capture from "@capture/Capture";
-import { SUB_COLOR } from "@utils/constant";
+import { MAIN_COLOR, SUB_COLOR } from "@utils/constant";
 import {
   ContentClip,
   ContentContainer,
   InnerContainer,
   MedicineBadgeContainer,
   PharmarcyContainer,
+  PharmarcyWrapper,
   Profile,
   SocialLoginState,
   UserInfo,
@@ -20,6 +23,13 @@ import {
 } from "./MyPage.style";
 import Medicine from "./medicine/Medicine";
 import Pharmarcy from "./pharmarcy/Pharmarcy";
+import Slider from "react-slick";
+import { useStyletron } from "styletron-react";
+
+interface pharmacyValues {
+  name: "동방 약국";
+  phnoeNumber: "010-0000-0000";
+}
 
 const medicinesName: { [key in string]: string } = {
   name1: "가스모틴정",
@@ -28,7 +38,45 @@ const medicinesName: { [key in string]: string } = {
   name4: "가스모틴정_4",
 };
 
+const pharmacyData: { [key in string]: pharmacyValues } = {
+  pharm_0: {
+    name: "동방 약국",
+    phnoeNumber: "010-0000-0000",
+  },
+  pharm_1: {
+    name: "동방 약국",
+    phnoeNumber: "010-0000-0000",
+  },
+  pharm_2: {
+    name: "동방 약국",
+    phnoeNumber: "010-0000-0000",
+  },
+  pharm_3: {
+    name: "동방 약국",
+    phnoeNumber: "010-0000-0000",
+  },
+  pharm_4: {
+    name: "동방 약국",
+    phnoeNumber: "010-0000-0000",
+  },
+  pharm_5: {
+    name: "동방 약국",
+    phnoeNumber: "010-0000-0000",
+  },
+};
+
+const settings = {
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  swipeToSlide: true,
+};
+
 function MyPage() {
+  const [css] = useStyletron();
   return (
     <Container>
       <InnerContainer>
@@ -68,8 +116,24 @@ function MyPage() {
         <ContentContainer $borderColor={SUB_COLOR}>
           <ContentClip $bgColor={SUB_COLOR}>관심 약국</ContentClip>
           <PharmarcyContainer>
-            <Pharmarcy />
-            <Pharmarcy />
+            <Slider
+              {...settings}
+              className={css({
+                width: "70vw",
+                height: "80%",
+                maxWidth: "400px",
+                margin: "0 auto",
+                border: `1px solid ${MAIN_COLOR}`,
+              })}>
+              {Object.entries(pharmacyData).map(([key, value], index) => (
+                <PharmarcyWrapper>
+                  <Pharmarcy
+                    name={value.name}
+                    phoneNumber={value.phnoeNumber}
+                  />
+                </PharmarcyWrapper>
+              ))}
+            </Slider>
           </PharmarcyContainer>
         </ContentContainer>
       </InnerContainer>
