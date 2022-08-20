6719d5c4a09ef31b778cc445fbefd4d8afa05951
Malza0408 <parkjh04080@gmail.com>
2022-08-21 02:38:50 +0900

    [Fix] Capture import 순서 변경

--- front/common/capture/Capture.tsx
+++ front/common/capture/Capture.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { useState } from "react";
+import Image from "next/image";
 import { CaptureContainer, CaptureButton } from "./Capture.style";
 
 function Capture() {
