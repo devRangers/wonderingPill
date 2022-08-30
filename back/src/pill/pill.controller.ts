9a127d502b0090dfd467c4cb72f18889ea355c50
ParkSuJeong74 <sujeong9158@gmail.com>
2022-08-30 11:20:17 +0900

    [Add] pill bookmark 생성

--- back/src/pill/pill.controller.ts
+++ back/src/pill/pill.controller.ts
@@ -5,18 +5,17 @@ import {
   Logger,
   Param,
   Put,
-  Query,
   UseGuards,
 } from '@nestjs/common';
 import {
   ApiCookieAuth,
   ApiOperation,
   ApiParam,
-  ApiQuery,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { GetCurrentUserId } from 'src/common/decorators';
+import { CommonResponseDto } from 'src/common/dto';
 import { AccessGuard } from 'src/common/guards';
 import { pillResultResponseDto } from './dto';
 import { PillService } from './pill.service';
@@ -27,67 +26,74 @@ export class PillController {
   private readonly logger = new Logger(`AlarmsController`);
   constructor(private readonly pillService: PillService) {}
 
-  @HttpCode(200)
-  @Get('search')
+  // @HttpCode(200)
+  // @Get('search')
+  // @ApiOperation({
+  //   summary: '약 검색필터 API',
+  //   description: '약 검색필터로 약을 검색한다.',
+  // })
+  // @ApiResponse({
+  //   status: 200,
+  //   description: '약 검색 성공',
+  //   // type: ,
+  // })
+  // @ApiQuery({
+  //   name: 'shape',
+  //   required: true,
+  //   description: '약 제형',
+  // })
+  // @ApiQuery({
+  //   name: 'colors',
+  //   required: true,
+  //   description: '약 색상',
+  // })
+  // @ApiQuery({
+  //   name: 'mark',
+  //   required: true,
+  //   description: '약 문양 여부',
+  // })
+  // @ApiQuery({
+  //   name: 'letters',
+  //   required: true,
+  //   description: '약 글자',
+  // })
+  // @ApiQuery({
+  //   name: 'name',
+  //   required: true,
+  //   description: '약 이름',
+  // })
+  // async searchPill(@Query() query) {
+  //   const pills = await this.pillService.searchPill(query);
+  //   this.logger.verbose(`Searching Pill Success`);
+  //   return {
+  //     statusCode: 200,
+  //     message: '약 검색 결과를 성공적으로 가져왔습니다.',
+  //     pills,
+  //   };
+  // }
+
+  @Put('bookmark/:id')
+  @UseGuards(AccessGuard)
   @ApiOperation({
-    summary: '약 검색필터 API',
-    description: '약 검색필터로 약을 검색한다.',
+    summary: '알약 북마크 API',
+    description: '알약을 북마크 한다.',
   })
   @ApiResponse({
     status: 200,
-    description: '약 검색 성공',
-    // type: ,
-  })
-  @ApiQuery({
-    name: 'shape',
-    required: true,
-    description: '약 제형',
-  })
-  @ApiQuery({
-    name: 'colors',
-    required: true,
-    description: '약 색상',
-  })
-  @ApiQuery({
-    name: 'mark',
-    required: true,
-    description: '약 문양 여부',
-  })
-  @ApiQuery({
-    name: 'letters',
-    required: true,
-    description: '약 글자',
-  })
-  @ApiQuery({
-    name: 'name',
-    required: true,
-    description: '약 이름',
-  })
-  async searchPill(@Query() query) {
-    const pills = await this.pillService.searchPill(query);
-    this.logger.verbose(`Searching Pill Success`);
-    return {
-      statusCode: 200,
-      message: '약 검색 결과를 성공적으로 가져왔습니다.',
-      pills,
-    };
-  }
-
-  @Put('bookmark/:')
-  @UseGuards(AccessGuard)
-  @ApiOperation({
-    summary: '알림 설정 API',
-    description: '알림 예약을 설정한다.',
-  })
-  @ApiResponse({
-    status: 201,
-    description: '알림 설정 성공',
-    // type: CommonResponseDto,
+    description: '알약 북마크 성공',
+    type: CommonResponseDto,
   })
   @ApiCookieAuth('accessToken')
   @ApiCookieAuth('refreshToken')
-  async bookmarkPill(@GetCurrentUserId() id: string) {
-    // 약 북마크
+  async bookmarkPill(
+    @GetCurrentUserId() id: string,
+    @Param('id') pill_id: number,
+  ): Promise<CommonResponseDto> {
+    await this.pillService.bookmarkPill(id, pill_id);
+    return {
+      statusCode: 200,
+      message: '북마크 설정을 완료했습니다.',
+    };
   }
 
   @HttpCode(200)
