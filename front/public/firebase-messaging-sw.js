4467ef08dcbd2f512b63547c03e4e9a3b5ceb21a
rnrn99 <giu0213@naver.com>
2022-09-02 10:22:49 +0900

    [Fix] event test 삭제

--- front/public/firebase-messaging-sw.js
+++ front/public/firebase-messaging-sw.js
@@ -23,12 +23,4 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log("onBackgroundMessage ", payload);
-  self.addEventListener("notificationclick", (event) => {
-    console.log("notificationclick in firebase sw ", event);
-    // self.clients.openWindow("/login");
-  });
-  self.addEventListener("click", (event) => {
-    console.log("click in firebase sw ", event);
-    // self.clients.openWindow("/login");
-  });
 });
