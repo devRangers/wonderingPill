d4559672031be8b4f126e9be6d8d8cda16c89448
rnrn99 <giu0213@naver.com>
2022-09-06 20:43:02 +0900

    [Fix] worker 삭제

--- front/worker/index.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("push", (event) => {
-  console.log("push!!!!! ", event);
-  // self.clients.openWindow("/login");
-});
-
-self.addEventListener("notificationclick", (event) => {
-  console.log("notificationclick!!!!! ", event);
-  // self.clients.openWindow("/login");
-});
