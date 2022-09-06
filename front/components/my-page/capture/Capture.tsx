38908200e5ee6e894b9454a8eb15203e6531b442
Malza0408 <parkjh04080@gmail.com>
2022-09-06 21:54:10 +0900

    [Fix] 불필요한 import 삭제

--- front/components/my-page/capture/Capture.tsx
+++ front/components/my-page/capture/Capture.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { useState } from "react";
 import { get, patch } from "@api";
 import { CaptureContainer, CaptureButton } from "./Capture.style";
 import { useAtom } from "jotai";
