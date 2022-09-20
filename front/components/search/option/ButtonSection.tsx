015f2569f22a6391f8a3ebf37b5d7c33104672c0
Malza0408 <parkjh04080@gmail.com>
2022-09-20 21:56:46 +0900

    [Add] search/option page의 알약 특징별 section 로직이 중복됨으로 Component로 분리

---
+++ front/components/search/option/ButtonSection.tsx
@@ -0,0 +1,43 @@
+import { GRAY_COLOR, SEMI_ACCENT_COLOR } from "@utils/constant";
+import React from "react";
+import { ButtonValue } from "./Option";
+import {
+  ButtonContainer,
+  OptionButton,
+  OptionContainer,
+  OptionTitle,
+} from "./Option.style";
+
+interface ButtonSectionProps {
+  title: string;
+  buttons: { [key in string]: ButtonValue };
+  isButtonSelected: boolean[];
+  handleClickButtonSelect: (index: number) => void;
+}
+
+function ButtonSection({
+  title,
+  buttons,
+  isButtonSelected,
+  handleClickButtonSelect,
+}: ButtonSectionProps) {
+  return (
+    <OptionContainer>
+      <OptionTitle $color={SEMI_ACCENT_COLOR}>{title}</OptionTitle>
+      <ButtonContainer>
+        {Object.entries(buttons).map(([key, value], index) => (
+          <OptionButton
+            key={key}
+            onClick={() => handleClickButtonSelect(index)}
+            $bgColor={SEMI_ACCENT_COLOR}
+            $isSelected={isButtonSelected[index]}
+            $color={GRAY_COLOR}>
+            {value.name}
+          </OptionButton>
+        ))}
+      </ButtonContainer>
+    </OptionContainer>
+  );
+}
+
+export default ButtonSection;
