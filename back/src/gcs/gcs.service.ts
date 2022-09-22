e060c8added61537920a500430bb5c8fdb14ee9f
ParkSuJeong74 <sujeong9158@gmail.com>
2022-09-22 23:57:06 +0900

    [Add] gcs set storage를 생성자에서 선언으로 변경

--- back/src/gcs/gcs.service.ts
+++ back/src/gcs/gcs.service.ts
@@ -1,29 +1,25 @@
 import { Storage } from '@google-cloud/storage';
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectThrottlerStorage } from '@nestjs/throttler';
 
 @Injectable()
 export class GcsService {
-  constructor(private readonly configService: ConfigService) {}
-
-  async setStorage() {
-    try {
-      const storage = new Storage({
-        keyFilename: `src/secure/${this.configService.get('GCS_KEY_FILE')}`,
-      });
-      return storage;
-    } catch (error) {
-      throw new ForbiddenException('외부 스토리지에 연결하지 못했습니다.');
-    }
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectThrottlerStorage() private readonly storage: Storage,
+  ) {
+    this.storage = new Storage({
+      keyFilename: `src/secure/${this.configService.get('GCS_KEY_FILE')}`,
+    });
   }
 
   async getPresignedUrl(id: string) {
     const fileName =
       '_' + new Date(Date.now()).getTime() + '_' + id + '_profile.png';
     const bucketName = 'wonderingpill-bucket/user_profileImg/';
-    const storage = await this.setStorage();
     try {
-      const url = await storage
+      const url = await this.storage
         .bucket(bucketName)
         .file(fileName)
         .getSignedUrl({
@@ -44,10 +40,9 @@ export class GcsService {
   async deleteImg(oldDate: string, id: string) {
     const fileName = '_' + oldDate + '_' + id + '_profile.png';
     const bucketName = 'wonderingpill-bucket/user_profileImg/';
-    const storage = await this.setStorage();
 
     try {
-      await storage.bucket(bucketName).file(fileName).delete({
+      await this.storage.bucket(bucketName).file(fileName).delete({
         ignoreNotFound: true,
       });
     } catch (error) {
