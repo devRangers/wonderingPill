f8d41a4ca49bb20938741b96263cf118ce88b788
ParkSuJeong74 <sujeong9158@gmail.com>
2022-10-05 21:06:43 +0900

    [Add] docker compose 위치 바꿈

--- back/src/utils/config.ts
+++ back/src/utils/config.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 
-const YAML_CONFIG_FILENAME = '../../docker-compose.yml';
+const YAML_CONFIG_FILENAME = '../../../docker-compose.yml';
 
 export default () => {
   return yaml.load(
