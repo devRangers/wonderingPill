06708e1a74a3ea2fdccbf1916baec1a2b0c23e2e
rnrn99 <giu0213@naver.com>
2022-10-06 21:45:25 +0900

    [Fix] prop 넘기기 전 타입 가드 세팅

--- front/pages/messages/setting/[id].tsx
+++ front/pages/messages/setting/[id].tsx
@@ -39,7 +39,13 @@ import RemindForm from "@messagesComp/setting/RemindForm";
 
 interface SetNotificationPageProps {
   bookmarkId: string;
-  setting: GetAlarmValues;
+  setting: {
+    minute: number;
+    hour: number;
+    vip: number[];
+    repeatTime: number;
+    pillName: string;
+  };
 }
 
 const SetNotificationPage: NextPage<SetNotificationPageProps> = ({
@@ -50,18 +56,10 @@ const SetNotificationPage: NextPage<SetNotificationPageProps> = ({
   const router = useRouter();
 
   const [isNotificationToggle, setIsNotificationToggle] = useState(true);
-  const [isRemindToggle, setIsRemindToggle] = useState(
-    typeof setting.repeatTime === "number" && setting.repeatTime > 0,
-  );
-  const [isAfternoon, setIsAfternoon] = useState(
-    typeof setting.hour === "number" && setting.hour >= 12,
-  );
-  const [vip, setVip] = useState<number[]>(
-    typeof setting.vip === "object" ? setting.vip : [],
-  );
-  const [pillName, setPillName] = useState(
-    typeof setting.pillName === "string" ? setting.pillName : "",
-  );
+  const [isRemindToggle, setIsRemindToggle] = useState(setting.repeatTime > 0);
+  const [isAfternoon, setIsAfternoon] = useState(setting.hour >= 12);
+  const [vip, setVip] = useState<number[]>(setting.vip);
+  const [pillName, setPillName] = useState(setting.pillName);
   const [deviceToken, setDeviceToken] = useState("");
 
   const setAlarmMutation = useMutation(
@@ -92,9 +90,9 @@ const SetNotificationPage: NextPage<SetNotificationPageProps> = ({
   );
 
   const initialValue: SettingFormValues = {
-    hour: typeof setting.hour === "number" ? setting.hour : 0,
-    minute: typeof setting.minute === "number" ? setting.minute : 0,
-    repeatTime: typeof setting.repeatTime === "number" ? setting.repeatTime : 0,
+    hour: setting.hour,
+    minute: setting.minute,
+    repeatTime: setting.repeatTime,
   };
 
   const formik = useFormik({
@@ -206,12 +204,18 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     };
   }
 
-  const result: GetAlarmResponse = await res.json();
+  const { alarm }: GetAlarmResponse = await res.json();
 
   return {
     props: {
       bookmarkId,
-      setting: result.alarm,
+      setting: {
+        minute: typeof alarm.minute === "number" ? alarm.minute : 0,
+        hour: typeof alarm.hour === "number" ? alarm.hour : 0,
+        vip: typeof alarm.vip === "object" ? alarm.vip : [],
+        repeatTime: typeof alarm.repeatTime === "number" ? alarm.repeatTime : 0,
+        pillName: typeof alarm.pillName === "string" ? alarm.pillName : "",
+      },
     },
   };
 };
