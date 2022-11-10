8ae156ec2c327299f2dcfbb825b4adf72719dd1b
Malza0408 <parkjh04080@gmail.com>
2022-11-10 21:42:45 +0900

    [Fix] Option page SSR
    새로고침시 query가 유지되지 않기에 SSR로 query를 유지시킴

--- front/pages/search/option/index.tsx
+++ front/pages/search/option/index.tsx
@@ -1,14 +1,33 @@
+import { GetServerSideProps, NextPage } from "next";
 import Option from "@searchComp/option/Option";
 import { NextSeo } from "next-seo";
 import { OPTION_SEO } from "next-seo.config";
 
-function OptionPage() {
+export interface OptionPageProps {
+  colors: string | string[];
+  letters: string;
+  shape: string;
+}
+
+const OptionPage: NextPage<OptionPageProps> = ({ colors, letters, shape }) => {
   return (
     <>
       <NextSeo {...OPTION_SEO} />
-      <Option />
+      <Option colors={colors} letters={letters} shape={shape} />
     </>
   );
-}
+};
 
 export default OptionPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { colors, letters, shape } = context.query;
+
+  return {
+    props: {
+      colors,
+      letters: typeof letters === "string" ? letters : "",
+      shape: shape ? shape : "circle",
+    },
+  };
+};
