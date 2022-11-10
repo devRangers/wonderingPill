17332940991997ddbd21a7eb054bce3b1a5d0842
Malza0408 <parkjh04080@gmail.com>
2022-11-10 20:59:02 +0900

    [Add] 알약 검색 실패 message 추가

--- front/common/utils/toastify.ts
+++ front/common/utils/toastify.ts
@@ -5,6 +5,8 @@ export const MESSAGE = {
   NEED_LOGIN: "로그인이 필요한 서비스입니다.",
   SAVE_ALARM: "알림을 설정했습니다.",
   CANCEL_ALARM: "알림을 취소했습니다.",
+  OPTION_SHAPE_FAIL: "체형을 하나 이상 선택해 주세요",
+  OPTION_COLOR_FAIL: "색상을 하나 이상 선택해 주세요",
   FAIL: "잠시 후 다시 시도해 주세요.",
 };
 
