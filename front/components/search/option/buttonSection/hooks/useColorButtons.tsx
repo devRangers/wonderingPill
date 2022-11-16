724edaae896e987230627dc19bb2198df0863f78
Malza0408 <parkjh04080@gmail.com>
2022-11-16 22:33:26 +0900

    [Fix] Option.tsx에서 모두 관리되던 각 섹션의 버튼들 분리
    hooks로 분리해서 필요한 부분만 메인(MainContainer)에서 가져와 사용

---
+++ front/components/search/option/buttonSection/hooks/useColorButtons.tsx
@@ -0,0 +1,41 @@
+import { useState, useCallback, useEffect } from "react";
+import { ButtonValue } from "@searchComp/option";
+import { changeStateWithQuery } from "@searchComp/option/MainContainer";
+
+function useColorButtons(colors: string | string[]) {
+  const [colorButtons, setColorButtons] = useState<{
+    [key in string]: ButtonValue;
+  }>({
+    red: { name: "빨강", isSelected: false },
+    orange: { name: "주황", isSelected: false },
+    yellow: { name: "노랑", isSelected: false },
+    green: { name: "초록", isSelected: false },
+    blue: { name: "파랑", isSelected: false },
+    purple: { name: "보라", isSelected: false },
+    brown: { name: "갈색", isSelected: false },
+    pink: { name: "분홍", isSelected: false },
+    white: { name: "하양", isSelected: false },
+    black: { name: "검정", isSelected: false },
+    gray: { name: "회색", isSelected: false },
+    transparent: { name: "투명", isSelected: false },
+  });
+
+  const handleSetColorButtons = useCallback((key: string) => {
+    setColorButtons((cur) => {
+      const curButtons = { ...cur };
+      curButtons[key].isSelected = !curButtons[key].isSelected;
+      return curButtons;
+    });
+  }, []);
+
+  useEffect(() => {
+    setColorButtons(changeStateWithQuery(colorButtons, colors));
+  }, []);
+
+  return {
+    colorButtons,
+    handleSetColorButtons,
+  };
+}
+
+export default useColorButtons;
---
+++ front/components/search/option/buttonSection/hooks/useMarkButtons.tsx
@@ -0,0 +1,35 @@
+import { useState, useCallback } from "react";
+import { ButtonValue } from "@searchComp/option";
+
+function useMarkButtons() {
+  const [markButtons, setColorButtons] = useState<{
+    [key in string]: ButtonValue;
+  }>({
+    mark: {
+      name: "있음",
+      isSelected: false,
+    },
+    noMark: {
+      name: "없음",
+      isSelected: true,
+    },
+  });
+
+  const handleSetMarkButtons = useCallback((key: string) => {
+    setColorButtons((cur) => {
+      const curButtons = { ...cur };
+      Object.entries(curButtons).forEach(
+        ([key, value], indedx) => (curButtons[key].isSelected = false),
+      );
+      curButtons[key].isSelected = !curButtons[key].isSelected;
+      return curButtons;
+    });
+  }, []);
+
+  return {
+    markButtons,
+    handleSetMarkButtons,
+  };
+}
+
+export default useMarkButtons;
