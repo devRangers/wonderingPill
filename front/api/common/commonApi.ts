e9a5600bafa4f0e06eb0603612520627ef8dbbb1
Malza0408 <parkjh04080@gmail.com>
2022-11-19 18:06:52 +0900

    [Fix] GCS에 PUT하는 API에 Content-Type 추가

--- front/api/common/commonApi.ts
+++ front/api/common/commonApi.ts
@@ -2,5 +2,8 @@ export const putImageOnGCS = (url: string, file: File) => {
   return fetch(url, {
     method: "PUT",
     body: file,
+    headers: {
+      "Content-Type": "application/octet-stream",
+    },
   });
 };
